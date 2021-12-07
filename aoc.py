0c6d98ac6ad09eb841ea9095af8c77c3ca8aae13
Wes Evans <euphwes@gmail.com>
2021-12-07 15:05:58 -0700

    Added a goofy top-level command to run all completed Advent of Code solutions in order, year by year then day by day.

--- aoc.py
+++ aoc.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import importlib
 
@@ -30,14 +31,54 @@ def __get_year_and_day_from_input():
     return year, day
 
 
-def __get_module_and_input_path():
+def __get_module_and_input_path(year=None, day=None):
     """ Returns the module for the specified year and day, and the path of the input file. """
 
-    year, day = __get_year_and_day_from_input()
+    if not year and not day:
+        year, day = __get_year_and_day_from_input()
+
     return __MODULE_NAME.format(year=year, day=day), __INPUT_PATH.format(year=year, day=day)
+
+
+def __get_all_years():
+    """ Returns all years I've participated in this Advent of Code repo. """
+
+    years = list()
+    for dir in os.listdir('.'):
+        try:
+            years.append(int(dir))
+        except ValueError:
+            pass
+    years.sort()
+    return years
+
+
+def __get_all_days_for_year(year):
+    """ Returns all days I've completed for the specified year. """
+
+    days = list()
+    for file in os.listdir(str(year)):
+        if not file.startswith('day_'):
+            continue
+        try:
+            cleaned_file = file.replace('day_','').replace('.py', '')
+            days.append(int(cleaned_file))
+        except ValueError:
+            pass
+    days.sort()
+    return days
 
 #---------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    module, input_file = __get_module_and_input_path()
-    importlib.import_module(module).run(input_file)
+
+    if 'all' in sys.argv:
+        for year in __get_all_years():
+            print('\n============\n    {}    \n============'.format(year))
+            for day in __get_all_days_for_year(year):
+                module, input_file = __get_module_and_input_path(year=year, day=day)
+                importlib.import_module(module).run(input_file)
+
+    else:
+        module, input_file = __get_module_and_input_path()
+        importlib.import_module(module).run(input_file)
