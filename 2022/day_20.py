d1604acedd02a72a651f993109f7fc2fbc7bccf7
Wes Evans <euphwes@gmail.com>
2022-12-20 21:23:38 -0700

    cleaned up but not commented version

--- 2022/day_20.py
+++ 2022/day_20.py
@@ -14,83 +14,48 @@ PART_TWO_DESCRIPTION = "Correct sum of the coordinates of the grove"
 PART_TWO_ANSWER = 14579387544492
 
 
-def _small_uuid():
+def _mini_uuid():
     return str(uuid4())[:8]
 
 
-@aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
-def part_one(stuff):
+def _translate_numbers(numbers):
+    original_ordered_numbers = copy(numbers)
 
-    size = len(stuff)
-    og_stuff = list(copy(stuff))
+    ix_to_uuid = {i: _mini_uuid() for i, _ in enumerate(numbers)}
+    uuid_to_ix = {v: k for k, v in ix_to_uuid.items()}
 
-    og_ix_to_uuid = {i: _small_uuid() for i, _ in enumerate(og_stuff)}
-    uuid_to_og_ix = {v: k for k, v in og_ix_to_uuid.items()}
-
-    stuff = [og_ix_to_uuid[i] for i in range(len(stuff))]
-
-    for og_value_ix in range(len(stuff)):
-
-        og_value = og_stuff[og_value_ix]
-        if og_value == 0:
-            continue
-
-        uuid_value = og_ix_to_uuid[og_value_ix]
-
-        is_moving_left = og_value < 0
-
-        og_value = og_value % (size - 1)
-        if is_moving_left:
-            while og_value >= 0:
-                og_value -= size - 1
-
-        i = stuff.index(uuid_value)
-        for n in range(abs(og_value)):
-            if is_moving_left:
-                a, b = stuff[i % size], stuff[(i - 1) % size]
-                stuff[i % size], stuff[(i - 1) % size] = b, a
-                i = (i - 1) % size
-
-            else:
-                a, b = stuff[i % size], stuff[(i + 1) % size]
-                stuff[i % size], stuff[(i + 1) % size] = b, a
-                i = (i + 1) % size
-
-        # print(f"\nmoved {og_value}\n")
-        # print(stuff)
-
-    stuff = [og_stuff[uuid_to_og_ix[uuid]] for uuid in stuff]
-    i = stuff.index(0)
-
-    # ? 14165 wrong swaps
-    # not 13972, 15077, -5345, -11007, 23161, 473
-    # print(stuff)
     return (
-        stuff[(i + 1000) % size] + stuff[(i + 2000) % size] + stuff[(i + 3000) % size]
+        [ix_to_uuid[i] for i in range(len(numbers))],
+        ix_to_uuid,
+        uuid_to_ix,
+        original_ordered_numbers,
     )
 
 
-@aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
-def part_two(stuff):
+def _translate_uuids(numbers, original_ordered_values, uuid_to_original_ix):
+    return [original_ordered_values[uuid_to_original_ix[uuid]] for uuid in numbers]
 
-    stuff = [n * 811589153 for n in stuff]
 
-    size = len(stuff)
-    og_stuff = list(copy(stuff))
+def _run_switcheroo(numbers, iterations=1, is_part_two=False):
+    if is_part_two:
+        numbers = [n * 811589153 for n in numbers]
+    (
+        translated,
+        ix_to_uuid,
+        uuid_to_ix,
+        og_ordered_numbers,
+    ) = _translate_numbers(numbers)
 
-    og_ix_to_uuid = {i: _small_uuid() for i, _ in enumerate(og_stuff)}
-    uuid_to_og_ix = {v: k for k, v in og_ix_to_uuid.items()}
+    size = len(translated)
 
-    stuff = [og_ix_to_uuid[i] for i in range(len(stuff))]
+    for _ in range(iterations):
+        for og_value_ix in range(len(translated)):
 
-    for _ in range(10):
-        for og_value_ix in range(len(stuff)):
-
-            og_value = og_stuff[og_value_ix]
+            og_value = og_ordered_numbers[og_value_ix]
             if og_value == 0:
                 continue
 
-            uuid_value = og_ix_to_uuid[og_value_ix]
+            uuid_value = ix_to_uuid[og_value_ix]
 
             is_moving_left = og_value < 0
 
@@ -99,23 +64,44 @@ def part_two(stuff):
                 while og_value >= 0:
                     og_value -= size - 1
 
-            i = stuff.index(uuid_value)
+            i = translated.index(uuid_value)
             for n in range(abs(og_value)):
                 if is_moving_left:
-                    a, b = stuff[i % size], stuff[(i - 1) % size]
-                    stuff[i % size], stuff[(i - 1) % size] = b, a
+                    a, b = translated[i % size], translated[(i - 1) % size]
+                    translated[i % size], translated[(i - 1) % size] = b, a
                     i = (i - 1) % size
 
                 else:
-                    a, b = stuff[i % size], stuff[(i + 1) % size]
-                    stuff[i % size], stuff[(i + 1) % size] = b, a
+                    a, b = translated[i % size], translated[(i + 1) % size]
+                    translated[i % size], translated[(i + 1) % size] = b, a
                     i = (i + 1) % size
 
-    stuff = [og_stuff[uuid_to_og_ix[uuid]] for uuid in stuff]
-    i = stuff.index(0)
+    return _translate_uuids(translated, og_ordered_numbers, uuid_to_ix)
+
+
+@aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
+def part_one(numbers):
+
+    numbers = _run_switcheroo(numbers, iterations=1, is_part_two=False)
+    i = numbers.index(0)
 
     return (
-        stuff[(i + 1000) % size] + stuff[(i + 2000) % size] + stuff[(i + 3000) % size]
+        numbers[(i + 1000) % len(numbers)]
+        + numbers[(i + 2000) % len(numbers)]
+        + numbers[(i + 3000) % len(numbers)]
+    )
+
+
+@aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
+def part_two(numbers):
+
+    numbers = _run_switcheroo(numbers, iterations=10, is_part_two=True)
+    i = numbers.index(0)
+
+    return (
+        numbers[(i + 1000) % len(numbers)]
+        + numbers[(i + 2000) % len(numbers)]
+        + numbers[(i + 3000) % len(numbers)]
     )
 
 
