c9dd3fe3ddfce7e9b3b800f1fae9ade815fec192
Wes Evans <euphwes@gmail.com>
2024-01-12 15:14:53 -0700

    Completed 2023 Day 15.

--- 2023/day_15.py
+++ 2023/day_15.py
@@ -4,14 +4,16 @@ from util.input import get_input
 DAY = 15
 YEAR = 2023
 
-PART_ONE_DESCRIPTION = ""
-PART_ONE_ANSWER = None
+PART_ONE_DESCRIPTION = "sum of HASH algorithm on each step of initialization seq"
+PART_ONE_ANSWER = 509167
 
-PART_TWO_DESCRIPTION = ""
-PART_TWO_ANSWER = None
+PART_TWO_DESCRIPTION = "focusing power of lens config"
+PART_TWO_ANSWER = 259333
 
 
 def _myhash(val):
+    """Implement the hashing algorithm described in the problem input."""
+
     curr = 0
     for c in val:
         curr += ord(c)
@@ -21,16 +23,15 @@ def _myhash(val):
 
 
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
-def part_one(stuff):
-    return sum(_myhash(s) for s in stuff[0].split(","))
+def part_one(initialization_sequence):
+    return sum(_myhash(s) for s in initialization_sequence[0].split(","))
 
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
-def part_two(stuff):
-    instrs = stuff[0].split(",")
-
+def part_two(initialization_sequence):
     boxes = {x: [] for x in range(256)}
 
+    instrs = initialization_sequence[0].split(",")
     for instr in instrs:
         if "-" in instr:
             label = instr.split("-")[0]
@@ -68,15 +69,6 @@ def part_two(stuff):
             raise ValueError(f"Unknown instruction: {instr}")
 
     score = 0
-
-    # for box in range(256):
-    #     lenses = boxes[box]
-    #     if lenses:
-    #         print()
-    #         print(f"box {box}")
-    #         print(lenses)
-
-    # back outside loop
     for box in range(256):
         boxvalue = box + 1
         lenses = boxes[box]
@@ -91,8 +83,8 @@ def part_two(stuff):
 
 
 def run(input_file):
-    stuff = get_input(input_file)
-    part_one(stuff)
+    initialization_sequence = get_input(input_file)
+    part_one(initialization_sequence)
 
-    stuff = get_input(input_file)
-    part_two(stuff)
+    initialization_sequence = get_input(input_file)
+    part_two(initialization_sequence)
