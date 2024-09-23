71410af8a5d4c8b42ac36fc64daea81ebaad3ff1
Wes Evans <euphwes@gmail.com>
2024-09-23 09:53:26 -0600

    wip refactoring to use angles and not slope, not working though

--- 2019/day_10.py
+++ 2019/day_10.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from itertools import combinations
-from math import inf, atan
+from math import inf, atan, pi
 
 from util.decorators import aoc_output_formatter
 from util.input import get_input
@@ -15,7 +15,7 @@ PART_TWO_DESCRIPTION = ""
 PART_TWO_ANSWER = None
 
 
-def _slope_between(ast1, ast2):
+def _angle_between(ast1, ast2):
     x1, y1 = ast1
     x2, y2 = ast2
 
@@ -23,9 +23,23 @@ def _slope_between(ast1, ast2):
     dy = y2 - y1
 
     if dx == 0:
-        return inf
+        angle_degrees = 90 if y2 > y1 else 270
+    elif dy == 0:
+        angle_degrees = 0 if x2 > x1 else 180
     else:
-        return dy / dx
+        angle_degrees = atan(dy / dx) * (180 / pi)
+        if x2 < x1:
+            angle_degrees += 180
+
+    # We want "0" to be "up" so let's adjust everything downwards by 90 degrees.
+    # TODO why
+    angle_degrees -= 90
+
+    # Now bring everything back into the range 0-360
+    if angle_degrees < 0:
+        angle_degrees += 360
+
+    return angle_degrees
 
 
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
@@ -43,38 +57,30 @@ def part_one(asteroid_map):
     for ast1, ast2 in combinations(asteroids_locations, 2):
         if ast1 == ast2:
             continue
-        slope = _slope_between(ast1, ast2)
-        asteroid_angles[ast1][slope].append(ast2)
-        asteroid_angles[ast2][slope].append(ast1)
+        asteroid_angles[ast1][_angle_between(ast1, ast2)].append(ast2)
+
+    from pprint import pprint
 
     best_count = 0
+    best_asteroid = None
+
     for asteroid, angle_dict in asteroid_angles.items():
+        # print(f"\nlooking at {asteroid}")
+        # pprint(dict(angle_dict))
 
         ax, ay = asteroid
 
         curr_count = 0
 
-        for angle, other_asts in angle_dict.items():
-            if angle == 0:
-                if any(bx > ax for bx, _ in other_asts):
-                    curr_count += 1
-                if any(bx < ax for bx, _ in other_asts):
-                    curr_count += 1
-            elif angle == inf:
-                if any(by > ay for _, by in other_asts):
-                    curr_count += 1
-                if any(by < ay for _, by in other_asts):
-                    curr_count += 1
-            else:
-                if any(bx > ax for bx, _ in other_asts):
-                    curr_count += 1
-                if any(bx < ax for bx, _ in other_asts):
-                    curr_count += 1
+        for _, other_asts in angle_dict.items():
+            if other_asts:
+                curr_count += 1
 
-        print(curr_count)
         if curr_count > best_count:
+            best_asteroid = asteroid
             best_count = curr_count
 
+    print(f"best asteroid is {best_asteroid}")
     return best_count
 
 
