28cf109db1c0753606ec6a778920fc6792ac7203
Wes Evans <euphwes@gmail.com>
2024-12-16 12:23:37 -0700

    add script to create empty files for full year, from template

--- aoc.py
+++ aoc.py
@@ -87,11 +87,48 @@ def __run_all():
     print("\n******** Finished running all completed Advent of Code problems! ********")
 
 
+def __create_year(year):
+    if not os.path.exists(year):
+        print(f"Creating directory for year {year}")
+        os.mkdir(year)
+
+    if not os.path.exists(f"{year}/inputs"):
+        print(f"Creating directory for inputs for year {year}")
+        os.mkdir(f"{year}/inputs")
+
+    for day in range(1, 25):
+        if not os.path.exists(f"{year}/day_{day}.py"):
+            print(f"Creating file for year {year}, day {day}")
+
+            # read day_n_template.py file to memory
+            file_contents = ""
+            with open("day_n_template.py", "r") as f:
+                file_contents = f.read()
+
+            # find the line containing DAY = 'REPLACE_ME' and replace the day number
+            file_contents = file_contents.replace("DAY = 'REPLACE_ME'", f"DAY = {day}")
+            # same for YEAR
+            file_contents = file_contents.replace("YEAR = 'REPLACE_ME'", f"YEAR = {year}")
+
+            # write the file contents to the new file
+            with open(f"{year}/day_{day}.py", "w") as f:
+                f.write(file_contents)
+
+        if not os.path.exists(f"{year}/inputs/input_day{day}.txt"):
+            print(f"Creating input file for year {year}, day {day}")
+            with open(f"{year}/inputs/input_day{day}.txt", "w") as f:
+                f.write("")
+
+
 # ----------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     if "all" in sys.argv:
         __run_all()
+
+    elif "create" == sys.argv[1]:
+        year = sys.argv[2]
+        __create_year(year)
 
     else:
         module, input_file = __get_module_and_input_path()
