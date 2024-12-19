ee477a7ff7853a40a8228a1c7b37a4cb15727fda
Wes Evans <euphwes@gmail.com>
2024-12-19 14:32:36 -0700

    wip 19, p1 done, wip p2

--- 2024/day_19.py
+++ 2024/day_19.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from util.decorators import aoc_output_formatter
 from util.input import get_input
 
@@ -11,14 +13,98 @@ PART_TWO_DESCRIPTION = ""
 PART_TWO_ANSWER = None
 
 
+def _parse_towel_patterns(raw_input: list[str]) -> frozenset[str]:
+    empty_line_ix = raw_input.index("")
+    towel_patterns = set()
+    for line in raw_input[:empty_line_ix]:
+        towel_patterns.update([pat.strip() for pat in line.split(",")])
+    return frozenset(towel_patterns)
+
+
+def _parse_designs(raw_input: list[str]) -> frozenset[str]:
+    empty_line_ix = raw_input.index("")
+    return frozenset(raw_input[empty_line_ix + 1 :])
+
+
+@cache
+def _is_valid_design(design: str, towel_patterns: frozenset[str]) -> bool:
+    if design in towel_patterns:
+        return True
+
+    for i in range(1, len(design)):
+        if _is_valid_design(design[:i], towel_patterns) and _is_valid_design(
+            design[i:],
+            towel_patterns,
+        ):
+            return True
+
+    return False
+
+
+@cache
+def _ways_to_make_design(
+    design: str,
+    towel_patterns: frozenset[str],
+) -> set[tuple[str]]:
+    # print(f'ways_to_make_design("{design}")')
+    if not _is_valid_design(design, towel_patterns):
+        return set()
+
+    combos = set()
+    if design in towel_patterns:
+        combos.add((design,))
+
+    for i in range(1, len(design)):
+        left = design[:i]
+        right = design[i:]
+
+        if not _is_valid_design(left, towel_patterns):
+            continue
+        if not _is_valid_design(right, towel_patterns):
+            continue
+
+        left_combos = _ways_to_make_design(left, towel_patterns)
+        right_combos = _ways_to_make_design(right, towel_patterns)
+
+        for left_combo in left_combos:
+            for right_combo in right_combos:
+                combo = left_combo + right_combo
+                combos.add(tuple(combo))
+
+    return combos
+
+
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
 def part_one(raw_input: list[str]) -> int | str | None:
-    pass
+    towel_patterns = _parse_towel_patterns(raw_input)
+    designs = _parse_designs(raw_input)
+    return sum(1 for design in designs if _is_valid_design(design, towel_patterns))
 
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
-    pass
+    towel_patterns = _parse_towel_patterns(raw_input)
+    designs = _parse_designs(raw_input)
+
+    foo = 0
+    try:
+        for design in designs:
+            # print(f"\n NEW LINE {design}")
+            foo += len(_ways_to_make_design(design, towel_patterns))
+        # capture ctrl-c
+    except KeyboardInterrupt:
+        pass
+
+    print()
+    print(_ways_to_make_design.__name__)
+    print(_ways_to_make_design.cache_info())
+
+    print()
+    print(_is_valid_design.__name__)
+    print(_is_valid_design.cache_info())
+    print()
+
+    return foo
 
 
 def run(input_file: str) -> None:
