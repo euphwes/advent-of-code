4c58a550db05587ce06224464d06553105bfbe6a
Wes Evans <euphwes@gmail.com>
2024-12-23 09:53:18 -0700

    day 21 complete

--- 2024/day_21.py
+++ 2024/day_21.py
@@ -1,5 +1,5 @@
-from collections import defaultdict
-from heapq import heappop, heappush
+from functools import cache
+from itertools import permutations
 
 from util.decorators import aoc_output_formatter
 from util.input import get_input
@@ -7,235 +7,248 @@ from util.input import get_input
 DAY = 21
 YEAR = 2024
 
-PART_ONE_DESCRIPTION = ""
-PART_ONE_ANSWER = None
+PART_ONE_DESCRIPTION = "complexity of keycodes with 3 directional keypads"
+PART_ONE_ANSWER = 157892
 
-PART_TWO_DESCRIPTION = ""
-PART_TWO_ANSWER = None
-
-NUMERIC_KEYPAD_GRAPH = {
-    "A": {
-        ("0", "<"),
-        ("3", "^"),
-    },
-    "0": {
-        ("A", ">"),
-        ("2", "^"),
-    },
-    "1": {
-        ("4", "^"),
-        ("2", ">"),
-    },
-    "2": {
-        ("1", "<"),
-        ("0", "v"),
-        ("3", ">"),
-        ("5", "^"),
-    },
-    "3": {
-        ("A", "v"),
-        ("2", "<"),
-        ("6", "^"),
-    },
-    "4": {
-        ("1", "v"),
-        ("5", ">"),
-        ("7", "^"),
-    },
-    "5": {
-        ("2", "v"),
-        ("6", ">"),
-        ("8", "^"),
-        ("4", "<"),
-    },
-    "6": {
-        ("3", "v"),
-        ("5", "<"),
-        ("9", "^"),
-    },
-    "7": {
-        ("4", "v"),
-        ("8", ">"),
-    },
-    "8": {
-        ("5", "v"),
-        ("7", "<"),
-        ("9", ">"),
-    },
-    "9": {
-        ("6", "v"),
-        ("8", "<"),
-    },
-}
-
-DIRECTIONAL_KEYPAD_GRAPH = {
-    "A": {
-        ("^", "<"),
-        (">", "v"),
-    },
-    "^": {
-        ("A", ">"),
-        ("v", "v"),
-    },
-    "<": {
-        ("v", ">"),
-    },
-    "v": {
-        ("<", "<"),
-        (">", ">"),
-        ("^", "^"),
-    },
-    ">": {
-        ("A", "^"),
-        ("v", "<"),
-    },
-}
-
-
-def _get_keypad_direction_chart(keypad):
-    directions = defaultdict(dict)
-
-    for start_key in keypad:
-        for end_key in keypad:
-            if start_key == end_key:
-                directions[start_key][end_key] = ""
-                continue
-
-            visited = set()
-
-            # queue is a list of (distance, num_dir_changes, key, path)
-            queue = []
-            heappush(queue, (0, start_key, ""))
-
-            while queue:
-                distance, key, path = heappop(queue)
-                if key in visited:
-                    continue
-                visited.add(key)
-
-                if key == end_key:
-                    # >>> sorted('^><v')
-                    # ['<', '>', '^', 'v']
-                    min_dir_changes_path = "".join(sorted(path))
-
-                    # if we're in the 1,4,7 column, we need to go right before going down
-                    if start_key in "147" and end_key in "0A":
-                        # print(
-                        #     f"\nstart: {start_key}, end: {end_key}, path: {min_dir_changes_path}",
-                        # )
-                        pass
-                        # shouldn't need to change anything
-
-                    # if we're in 0,A and going to 1,4,7, need to go up before going left
-                    elif start_key in "0A" and end_key in "147":
-                        # print(
-                        #     f"\nstart: {start_key}, end: {end_key}, path: {min_dir_changes_path}",
-                        # )
-                        # reverse path
-                        min_dir_changes_path = min_dir_changes_path[::-1]
-                        # print(
-                        #     f"\nstart: {start_key}, end: {end_key}, path: {min_dir_changes_path}",
-                        # )
-
-                    elif start_key in "^A" and end_key == "<":
-                        # need to go down before going left
-                        min_dir_changes_path = min_dir_changes_path[::-1]
-
-                    elif start_key == "<" and end_key in "^A":
-                        # need to go right before going up
-                        pass
-
-                    if start_key == "A" and end_key == "5":
-                        # TEST CASE
-                        # reverse this even though it doesn't need it
-                        min_dir_changes_path = min_dir_changes_path[::-1]
-
-                    directions[start_key][end_key] = min_dir_changes_path
-                    break
-
-                for neighbor_key, neighbor_direction in keypad[key]:
-                    if neighbor_key not in visited:
-                        new_path = path + neighbor_direction
-                        heappush(
-                            queue,
-                            (
-                                distance + 1,
-                                neighbor_key,
-                                new_path,
-                            ),
-                        )
-
-    return directions
+PART_TWO_DESCRIPTION = "complexity of keycodes with 26 directional keypads"
+PART_TWO_ANSWER = 197015606336332
 
 
 def _p(val):
     print(val)
 
 
-def _debug_paths(target_path, new_path):
-    target_path = list(target_path)
+NUMERIC_KEYPAD = [
+    "789",
+    "456",
+    "123",
+    " 0A",
+]
+NUMERIC_KEYPAD_COORDS = {
+    char: (x, y) for y, row in enumerate(NUMERIC_KEYPAD) for x, char in enumerate(row)
+}
 
-    target_to_print = ""
-    new_to_print = ""
+DIRECTIONAL_KEYPAD = [
+    " ^A",
+    "<v>",
+]
+DIRECTIONAL_KEYPAD_COORDS = {
+    char: (x, y) for y, row in enumerate(DIRECTIONAL_KEYPAD) for x, char in enumerate(row)
+}
 
-    for chunk in new_path.split("A")[:-1]:
-        new_to_print += chunk + "A  "
-        target_to_print += (" " * len(chunk)) + target_path.pop(0) + "  "
 
-    print(f"\n{target_to_print}\n{new_to_print}")
+def _does_path_go_through_numeric_blank(scoord, path):
+    sx, sy = scoord
+    for char in path:
+        if char == ">":
+            sx += 1
+        elif char == "<":
+            sx -= 1
+        elif char == "v":
+            sy += 1
+        elif char == "^":
+            sy -= 1
+        if NUMERIC_KEYPAD[sy][sx] == " ":
+            return True
+    return False
 
 
-def _solve_keycode(keycode, numeric_keypad_paths, directional_keypad_paths):
-    _p(f"\n==================\nsolving keycode: {keycode}")
-    path = ""
-    curr_char = "A"
+def _does_path_go_through_directional_blank(scoord, path):
+    sx, sy = scoord
+    for char in path:
+        if char == ">":
+            sx += 1
+        elif char == "<":
+            sx -= 1
+        elif char == "v":
+            sy += 1
+        elif char == "^":
+            sy -= 1
+        if DIRECTIONAL_KEYPAD[sy][sx] == " ":
+            return True
+    return False
 
-    for char in keycode:
-        path += numeric_keypad_paths[curr_char][char]
-        path += "A"
-        curr_char = char
 
-    paths = []
-    paths.append(path)
-    _debug_paths(keycode, path)
+@cache
+def _shortest_len_to_press_directional(
+    path: str,
+    num_directional_keypads_remaining: int = 3,
+) -> int:
+    # Let's say we're getting the path <^^A
+    # From A to < we can go
+    #     v<<, or <<v, or <v<, and then press A
+    # From < to ^ we can go
+    #     >^, or ^> ,and then press A
+    # From ^ to ^ we don't need to go anywhere, just press A
+    # From ^ to A we can go
+    #     >, then press A
+    # print()
+    indent = "   " * (3 - num_directional_keypads_remaining)
 
-    for _ in range(2):
-        target_path = paths[-1]
-        curr_char = "A"
-        next_path = ""
+    min_path_lengths = 0
 
-        for char in target_path:
-            next_path += directional_keypad_paths[curr_char][char]
-            next_path += "A"
-            curr_char = char
+    start_char = "A"
+    for target_char in path:
+        start_coord = DIRECTIONAL_KEYPAD_COORDS[start_char]
+        target_coord = DIRECTIONAL_KEYPAD_COORDS[target_char]
 
-        paths.append(next_path)
-        _debug_paths(target_path, next_path)
+        dx = target_coord[0] - start_coord[0]
+        dy = target_coord[1] - start_coord[1]
 
-    length = len(paths[-1])
-    keycode_digits = int("".join(c for c in keycode if c.isdigit()))
-    print(f"\n{length}*{keycode_digits}")
-    return length * keycode_digits
+        soln_base = ""
+        if dx > 0:
+            soln_base += ">" * dx
+        elif dx < 0:
+            soln_base += "<" * abs(dx)
+        if dy > 0:
+            soln_base += "v" * dy
+        elif dy < 0:
+            soln_base += "^" * abs(dy)
+
+        if num_directional_keypads_remaining == 1:
+            min_path_lengths += len(soln_base) + 1
+            # _p(f"{indent+'   '}{start_char} to {target_char}, cost={len(soln_base) + 1}")
+        else:
+            min_path = min(
+                _shortest_len_to_press_directional(
+                    "".join(path) + "A",
+                    num_directional_keypads_remaining - 1,
+                )
+                for path in permutations(soln_base)
+                if not _does_path_go_through_directional_blank(start_coord, path)
+            )
+            # _p(f"{indent+'   '}{start_char} to {target_char}, cost={min_path}")
+            min_path_lengths += min_path
+
+        start_char = target_char
+
+    # _p(
+    #     f"{indent}path: {path}, num_keypads: {num_directional_keypads_remaining}, "
+    #     f"best_cost: {min_path_lengths}",
+    # )
+    return min_path_lengths
+
+
+def _shortest_len_to_press_numeric(
+    start_char: str,
+    target_char: str,
+    num_directional_keypads: int = 3,
+) -> int:
+    start_coord = NUMERIC_KEYPAD_COORDS[start_char]
+    target_coord = NUMERIC_KEYPAD_COORDS[target_char]
+
+    dx = target_coord[0] - start_coord[0]
+    dy = target_coord[1] - start_coord[1]
+
+    soln_base = ""
+    if dx > 0:
+        soln_base += ">" * dx
+    elif dx < 0:
+        soln_base += "<" * abs(dx)
+    if dy > 0:
+        soln_base += "v" * dy
+    elif dy < 0:
+        soln_base += "^" * abs(dy)
+
+    min_path = min(
+        _shortest_len_to_press_directional("".join(path) + "A", num_directional_keypads)
+        for path in permutations(soln_base)
+        if not _does_path_go_through_numeric_blank(start_coord, path)
+    )
+    # _p(f"\n{start_char} to {target_char}, min_path: {min_path}")
+    return min_path
 
 
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
 def part_one(raw_input: list[str]) -> int | str | None:
-    numeric_keypad_paths = _get_keypad_direction_chart(NUMERIC_KEYPAD_GRAPH)
-    directional_keypad_paths = _get_keypad_direction_chart(DIRECTIONAL_KEYPAD_GRAPH)
+    complexity = 0
 
-    print("==============")
-    return sum(
-        _solve_keycode(keycode, numeric_keypad_paths, directional_keypad_paths)
-        for keycode in raw_input
-    )
+    for keycode in raw_input:
+        # _p(f"\n===============\nLooking at keycode: {keycode}")
+        num_of_final_keypad_presses = 0
+
+        start_char = "A"
+        for target_char in keycode:
+            num_of_final_keypad_presses += _shortest_len_to_press_numeric(
+                start_char,
+                target_char,
+                num_directional_keypads=2,
+            )
+            start_char = target_char
+
+        complexity += num_of_final_keypad_presses * int(
+            "".join(d for d in keycode if d.isdigit()),
+        )
+        # _p(
+        #     f"Keycode: {keycode}, keypresses: {num_of_final_keypad_presses}, "
+        #     f"value: {"".join(d for d in keycode if d.isdigit())}, complexity: {complexity}",
+        # )
+
+    return complexity
 
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
-    pass
+    complexity = 0
+
+    for keycode in raw_input:
+        # _p(f"\n===============\nLooking at keycode: {keycode}")
+        num_of_final_keypad_presses = 0
+
+        start_char = "A"
+        for target_char in keycode:
+            num_of_final_keypad_presses += _shortest_len_to_press_numeric(
+                start_char,
+                target_char,
+                num_directional_keypads=25,
+            )
+            start_char = target_char
+
+        complexity += num_of_final_keypad_presses * int(
+            "".join(d for d in keycode if d.isdigit()),
+        )
+        # _p(
+        #     f"Keycode: {keycode}, keypresses: {num_of_final_keypad_presses}, "
+        #     f"value: {"".join(d for d in keycode if d.isdigit())}, complexity: {complexity}",
+        # )
+
+    return complexity
 
 
 def run(input_file: str) -> None:
     # 161468 too high
     part_one(get_input(input_file))
     part_two(get_input(input_file))
+
+
+assert """
+==================
+solving keycode: 508A
+
+   5    0     8      A
+<^^A  vvA  ^^^A  >vvvA
+
+   <    ^  ^   A    v  v    A   ^  ^  ^   A   >   v  v  v    A
+v<<A  >^A  A  >A  <vA  A  >^A  <A  A  A  >A  vA  <A  A  A  >^A
+              10
+
+  v   <  <     A   >    ^   A  A   >   A     <   v    A  A   >    ^   A     <     A  A  A   >   A    v    A     <     A  A  A   >    ^   A
+<vA  <A  A  >>^A  vA  <^A  >A  A  vA  ^A  v<<A  >A  >^A  A  vA  <^A  >A  v<<A  >>^A  A  A  vA  ^A  <vA  >^A  v<<A  >>^A  A  A  vA  <^A  >A
+                                      22
+76*508
+
+
+==================
+solving keycode: 508A
+
+   5    0     8      A
+^^<A  vvA  ^^^A  >vvvA
+
+ ^  ^    <     A    v  v    A   ^  ^  ^   A   >   v  v  v    A
+<A  A  v<A  >>^A  <vA  A  >^A  <A  A  A  >A  vA  <A  A  A  >^A
+              10
+
+   <     A  A    v   <     A   >  >    ^   A     <   v    A  A   >    ^   A     <     A  A  A   >   A    v    A     <     A  A  A   >    ^   A
+v<<A  >>^A  A  <vA  <A  >>^A  vA  A  <^A  >A  v<<A  >A  >^A  A  vA  <^A  >A  v<<A  >>^A  A  A  vA  ^A  <vA  >^A  v<<A  >>^A  A  A  vA  <^A  >A
+                                          26
+"""  # noqa: PLW0129, S101
