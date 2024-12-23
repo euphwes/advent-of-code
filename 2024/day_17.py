a27709b03d3d0b175452babf50260b246fc301b4
Wes Evans <euphwes@gmail.com>
2024-12-23 16:34:10 -0700

    Revert "wip explanation before going back to previous commit"
    This reverts commit db581520715588fe334cb87a1dec9a9bfaa6e448.

--- 2024/day_17.py
+++ 2024/day_17.py
@@ -1,5 +1,6 @@
 from util.decorators import aoc_output_formatter
 from util.input import get_input
+from util.iter import int_stream
 
 from .computer import Computer
 
@@ -25,36 +26,29 @@ def part_one(raw_input: list[str]) -> int | str | None:
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
-    # Program:
-    # 2,4
-    # 1,7
-    # 7,5
-    # 1,7
-    # 0,3
-    # 4,1
-    # 5,5
-    # 3,0
-
-    # ---------
-    # add assembly instructions
-
-    # 2,4 (BST) B = A % 8      || take lower 3 bits of A and store in B
-    # 1,7 (BXL) B = B ^ 7      || 7 in binary is 111, so toggle the lower 3 bits of B
-    # 7,5 (CDV) C = A // (2^B) || divide A by 2^B and store in C
-    # 1,7 (BXL) B = B ^ 7      || toggle the lower 3 bits of B again
-    # 0,3 (ADV) A = A // (2^B) || divide A by 2^B and store in A
-    # 4,1 (BXC) B = B ^ C      || B = B ^ C
-    # 5,5 (OUT) output (B % 8) || output the lower 3 bits of B
-    # 3,0 (JNZ) if A != 0, jump to 0
-
-    # ------------
-    # ok let's work backwords to figure out the value of register A
-    # to get the program to output itself
-    #
-    # final value output is [ 0 ]
-    # so lower 3 bits of B = 000
-
-    return 0
+    # Program: 2,4,1,7,7,5,1,7,0,3,4,1,5,5,3,0
+    # 2100000000
+    f = []
+    for i in int_stream(end=100):
+        computer = Computer(
+            program=[2, 4, 1, 7, 7, 5, 1, 7, 0, 3, 4, 1, 5, 5, 3, 0],
+            initial_registers={
+                "A": i,
+                "B": 0,
+                "C": 0,
+            },
+        )
+        raw_output = [str(n) for n in computer.execute()]
+        output = ",".join(raw_output)
+        output_b8_to_b10 = int("".join(raw_output), 8)
+        f.append(str(output_b8_to_b10))
+        # if i % 100000 == 0:
+        # print(f"\nTrying i={i}, output={output}, b8={output_b8_to_b10}")
+        # print(rf"i={i}, b8={output_b8_to_b10}")
+        if output == "2,4,1,7,7,5,1,7,0,3,4,1,5,5,3,0":
+            return i
+    print(",".join(f))
+    raise RuntimeError("No solution found")
 
 
 def run(input_file: str) -> None:
