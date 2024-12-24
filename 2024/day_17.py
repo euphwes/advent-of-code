c264934dfd85e6ca28d38c62bb12e913d5400e0f
Wes Evans <euphwes@gmail.com>
2024-12-24 13:57:59 -0700

    wip

--- 2024/day_17.py
+++ 2024/day_17.py
@@ -36,101 +36,66 @@ def _compare_output(output: list[int], target_program: list[int]) -> int:
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
     target_program = [2, 4, 1, 7, 7, 5, 1, 7, 0, 3, 4, 1, 5, 5, 3, 0]
-    # target_program = [0, 3, 5, 4, 3, 0]
+    target_length = len(target_program)
 
-    def _run(register_a):
-        return Computer(
-            program=target_program,
-            initial_registers={
-                "A": register_a,
-                "B": 0,
-                "C": 0,
-            },
-        ).execute()
+    # Program:
+    # 2,4
+    # 1,7
+    # 7,5
+    # 1,7
+    # 0,3
+    # 4,1
+    # 5,5
+    # 3,0
 
-    # Program: 2,4,1,7,7,5,1,7,0,3,4,1,5,5,3,0
-    # 2100000000
-    # for i in int_stream():
-    #     computer = Computer(
-    #         program=target_program,
-    #         initial_registers={
-    #             "A": i,
-    #             "B": 0,
-    #             "C": 0,
-    #         },
-    #     )
-    #     raw_output = computer.execute()
-    #     print(f"i={i}, output={','.join([str(n) for n in raw_output])}")
-    #     if raw_output == target_program:
-    #         return i
+    # ---------
+    # add assembly instructions
 
-    sa = 1
-    while True:
-        output = _run(sa)
-        if _compare_output(output, target_program) == -1:
-            sa *= 10
-        elif _compare_output(output, target_program) == 1:
-            sa /= 5
-        else:
-            break
-    sa = int(sa)
+    # 2,4 (BST) B = A % 8      || take lower 3 bits of A and store in B
+    # 1,7 (BXL) B = B ^ 7      || 7 in binary is 111, so toggle the lower 3 bits of B
+    # 7,5 (CDV) C = A // (2^B) || divide A by 2^B and store in C
+    # 1,7 (BXL) B = B ^ 7      || toggle the lower 3 bits of B again
+    # 0,3 (ADV) A = A // 8     || divide A by 8 and store in A
+    # 4,1 (BXC) B = B ^ C      || B = B ^ C
+    # 5,5 (OUT) output (B % 8) || output the lower 3 bits of B
+    # 3,0 (JNZ) if A != 0, jump to 0
 
-    print(f"right order of magnitude with register A = {sa}")
+    # ------------
+    #
+    # while A != 0:
+    #   B = A % 8      # B is the lowest 3 bits of A
+    #   B = B ^ 7      # toggle the lowest 3 bits of B
+    #
+    #   C = A // 2^B   # C = A // 2^B  (bit shift right by B)
+    #   B = B ^ 7      # toggle the lowest 3 bits of B again
+    #
+    #   A = A // 8     # bit shift right by 3 bits
+    #   B = B ^ C      # B = B XOR C, aka toggle some bits of B based on C
+    #   output B % 8   # output the lowest 3 bits of B
 
-    # increment = sa // 100000
-    # while True:
-    #     str_output = ",".join([str(n) for n in _run(sa)])
-    #     # if not str_output.endswith("5,1,7,0,3,4,1,5,5,3,0"):
-    #     # if not str_output.endswith("1,5,5,3,0"):  # with inc = sa // 100
-    #     # if not str_output.endswith("3,4,1,5,5,3,0"):  # with inc = sa // 10000 ** this works
-    #     if not str_output.endswith(
-    #         "0,3,4,1,5,5,3,0"
-    #     ):  # with inc = sa // 100000 ** this works in 90s
-    #         sa += increment
-    #         continue
-    #     break
+    def _debug(a):
+        a_in_binary = bin(a)[2:]
+        print()
+        print(f"A: {a} (binary: {a_in_binary})")
 
-    # right order of magnitude with register A = 100000000000000
-    # startswith "0,3,4,1,5,5,3,0"
-    # maybe getting closer? sa=2120454000000000 and str_output='0,7,5,1,5,2,6,6,1,0,3,4,1,5,5,3,0'
+        output = []
+        while True:
+            b = a % 8
+            b ^= 7
+            c = a // (2**b)
+            b ^= 7
+            a //= 8
+            b ^= c
+            output.append(b % 8)
+            if a == 0:
+                break
+        assert len(output) == target_length
+        by_program = ",".join([str(n) for n in output])
+        print(f"Output: {by_program}")
 
-    sa = 2120454000000000
-    increment = sa // 100_000_000
-
-    while True:
-        str_output = ",".join([str(n) for n in _run(sa)])
-        if not str_output.endswith(
-            "7,0,3,4,1,5,5,3,0",  # increment = sa // 100_000_000
-        ):
-            sa += increment
-            continue
-        break
-
-    print(f"\nmaybe getting closer? {sa=}, {increment=} and {str_output=}")
-
-    # sa = 2120454000000000
-    # increment = sa // 100_000_000  --> 21204540
-    # --->
-    # sa=2120454254454480
-    # str_output='5,4,2,0,5,3,5,6,7,0,3,4,1,5,5,3,0'
-    #               need lower?   *
-
-    sa = 2120454000000000
-    increment = sa // 1_000_000_000
-
-    while True:
-        str_output = ",".join([str(n) for n in _run(sa)])
-        print(f"{sa=}, {str_output=}")
-        if str_output.endswith(
-            "1,7,0,3,4,1,5,5,3,0",
-        ):
-            break
-        sa += increment
-        break
-        continue
-
-    print(f"\nmaybe getting closer? {sa=}, {increment=} and {str_output=}")
-    return
+    _debug(int("1000000000000000000000000000000000000000000000", 2))
+    _debug(int("1000000000000000000000000000000000000000000001", 2))
+    _debug(int("1111111111111111000000100000100000010100000000", 2))
 
 
 def run(input_file: str) -> None:
