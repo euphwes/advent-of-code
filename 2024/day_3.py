4012437a7708c65c590de7abd96cf3be2ea08bed
Wes Evans <euphwes@gmail.com>
2025-03-14 10:50:11 -0600

    Completed 2024 Day 3.

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -1,77 +1,59 @@
+import re
+from math import prod
+
 from util.decorators import aoc_output_formatter
 from util.input import get_input
-
-import re
 
 DAY = 3
 YEAR = 2024
 
-PART_ONE_DESCRIPTION = ""
-PART_ONE_ANSWER = None
+PART_ONE_DESCRIPTION = "sum of all multiplication instructions"
+PART_ONE_ANSWER = 191183308
 
-PART_TWO_DESCRIPTION = ""
-PART_TWO_ANSWER = None
+PART_TWO_DESCRIPTION = "sum of all enabled multiplication instructions"
+PART_TWO_ANSWER = 92082041
+
+
+def _evaluate_mul(raw_instruction: str) -> int:
+    """Evaluate a raw "mul(n, m)" instruction, returning n*m."""
+
+    return prod(int(n) for n in raw_instruction[4:-1].split(","))
 
 
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
-def part_one(stuff):
-
-    pattern = r"mul\(\d{1,3}\,\d{1,3}\)"
-
-    n = 0
-
-    for line in stuff:
-        matches = re.findall(pattern, line)
-        for match in matches:
-            # print()
-            # print(match)
-            a, b = (int(n) for n in match[4:-1].split(","))
-            n += a * b
-            # print(f"adding {a*b}, sum now {n}")
-
-    return n
+def part_one(raw_input: list[str]) -> int | str | None:
+    return sum(
+        _evaluate_mul(match)
+        for line in raw_input
+        # Extract matches of "mul(n, m)" in each line of the input
+        for match in re.findall(r"mul\(\d{1,3}\,\d{1,3}\)", line)
+    )
 
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
-def part_two(stuff):
-    pattern = r"mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)"
-
+def part_two(raw_input: list[str]) -> int | str | None:
     n = 0
 
     enabled = True
-
-    for line in stuff:
-        matches = re.findall(pattern, line)
-        for match in matches:
-            print(match)
+    for line in raw_input:
+        # Extract matches of "mul(n, m)", "do()", and "don't()"
+        for match in re.findall(r"mul\(\d{1,3}\,\d{1,3}\)|do\(\)|don't\(\)", line):
+            # If the match is "do()", enable any following multiplication matches
             if match == "do()":
                 enabled = True
                 continue
+
+            # If the match is "don't()", disable any following multiplication matches
             if match == "don't()":
                 enabled = False
                 continue
 
-            if not enabled:
-                continue
-
-            # print()
-            # print(match)
-            a, b = (int(n) for n in match[4:-1].split(","))
-            n += a * b
-            # print(f"adding {a*b}, sum now {n}")
+            # Only sum the product of a "mul(n, m)" match if multiplication is enabled
+            n += _evaluate_mul(match) if enabled else 0
 
     return n
 
 
-# ----------------------------------------------------------------------------------------------
-
-
-def run(input_file):
-    stuff = get_input(input_file)
-    # not 32081624
-    # 32081624
-    # 191183308
-    part_one(stuff)
-
-    stuff = get_input(input_file)
-    part_two(stuff)
+def run(input_file: str) -> None:
+    part_one(get_input(input_file))
+    part_two(get_input(input_file))
