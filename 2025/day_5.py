80bb7862cf36f325dca9a3f40e8133e4b1a0d132
Wes Evans <euphwes@gmail.com>
2025-11-30 15:29:49 -0700

    Add generated scaffolds for 2025.

---
+++ 2025/day_5.py
@@ -0,0 +1,26 @@
+from util.decorators import aoc_output_formatter
+from util.input import get_input
+
+DAY = 5
+YEAR = 2025
+
+PART_ONE_DESCRIPTION = ""
+PART_ONE_ANSWER = None
+
+PART_TWO_DESCRIPTION = ""
+PART_TWO_ANSWER = None
+
+
+@aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
+def part_one(raw_input: list[str]) -> int | str | None:
+    pass
+
+
+@aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
+def part_two(raw_input: list[str]) -> int | str | None:
+    pass
+
+
+def run(input_file: str) -> None:
+    part_one(get_input(input_file))
+    part_two(get_input(input_file))
