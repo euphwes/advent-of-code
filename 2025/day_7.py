012e27e00c27a72f4408274518a2bde670c4d341
Wes Evans <euphwes@gmail.com>
2025-12-08 09:44:52 -0700

    done p2

--- 2025/day_7.py
+++ 2025/day_7.py
@@ -1,7 +1,5 @@
 from collections import defaultdict
-from dataclasses import dataclass
 from functools import cache
-from typing import Self
 
 from util.decorators import aoc_output_formatter
 from util.input import get_input
@@ -67,16 +65,6 @@ def part_one(raw_input: list[str]) -> int | str | None:
     return count
 
 
-@dataclass
-class Node:
-    coord: Coord
-    children: list[Self]
-
-    @property
-    def num_paths(self) -> int:
-        return 0
-
-
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
     grid = _parse_grid(raw_input)
@@ -85,6 +73,23 @@ def part_two(raw_input: list[str]) -> int | str | None:
     for coord, item in grid.items():
         if item == "^":
             splitters.add(coord)
+
+    # Remove splitters which are "inaccessible"
+    splitters_to_remove = []
+    for scoord in splitters:
+        x, y = scoord
+        while y > 0:
+            y -= 1
+            if (x - 1, y) in splitters:
+                break
+            if (x + 1, y) in splitters:
+                break
+            if (x, y) in splitters:
+                splitters_to_remove.append(scoord)
+                break
+
+    for s in splitters_to_remove:
+        splitters.remove(s)
 
     sources: set[Coord] = set()
     for coord, item in grid.items():
@@ -106,23 +111,37 @@ def part_two(raw_input: list[str]) -> int | str | None:
     def _count_paths(c: Coord) -> int:
         cx, cy = c
         if cy == deepest_y:
-            print(f"{c} is deepest, returning 1")
+            # print(f"{c} is a leaf")
             return 1
 
         children = []
 
-        while cy < deepest_y:
+        while cy <= deepest_y:
             cy += 2
-            if (cx - 1, cy) in sources:
+            done = False
+            if (cx, cy) in splitters and (cx - 1, cy) in sources:
                 children.append((cx - 1, cy))
+                done = True
+            if (cx, cy) in splitters and (cx + 1, cy) in sources:
                 children.append((cx + 1, cy))
+                done = True
+            if done:
                 break
 
+        if not children:
+            return 1
+
         s = sum(_count_paths(child) for child in children)
-        print(f"{c} has children {children}, returning {s}")
+        # print(f"{c} has children {children}, returning {s}")
         return s
 
-    return _count_paths(sources_by_y[0][0])
+    # for yval in sorted(sources_by_y.keys(), reverse=True):
+    #     # print(f"\nEvaluating coords at y={yval}")
+    #     for c in sources_by_y[yval]:
+    #         count = _count_paths(c)
+    #         # print(f"{c}: {count}")
+
+    return f"{_count_paths(sources_by_y[0][0]):_}"
 
 
 def run(input_file: str) -> None:
