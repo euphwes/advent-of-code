cd2dfda4d8be95f814828c05acfedd72c8d6d629
Wes Evans <euphwes@gmail.com>
2025-12-09 00:07:58 -0700

    wip part 2 day 9

--- 2025/day_9.py
+++ 2025/day_9.py
@@ -1,3 +1,7 @@
+from collections.abc import Callable
+from functools import cache
+from itertools import combinations
+
 from util.decorators import aoc_output_formatter
 from util.input import get_input
 
@@ -10,15 +14,303 @@ PART_ONE_ANSWER = None
 PART_TWO_DESCRIPTION = ""
 PART_TWO_ANSWER = None
 
+Coord = tuple[int, int]
+Range = tuple[Coord, Coord]
+
+
+def _parse(lines):
+    foo = []
+    for line in lines:
+        a, b = line.split(",")
+        foo.append((int(a), int(b)))
+    return foo
+
 
 @aoc_output_formatter(YEAR, DAY, 1, PART_ONE_DESCRIPTION, assert_answer=PART_ONE_ANSWER)
 def part_one(raw_input: list[str]) -> int | str | None:
-    pass
+    coords = _parse(raw_input)
+
+    m = -1
+    for c1, c2 in combinations(coords, 2):
+        x1, y1 = c1
+        x2, y2 = c2
+
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+
+        m = max((dx + 1) * (dy + 1), m)
+
+    return m
+
+
+def _get_is_in_shape_fn(perimeter_ranges: list[Range]) -> Callable[[Coord], bool]:
+    """Get a thing."""
+
+    # Remember corners to facilitate deciding if we're entering or exiting.
+    corners_dict: dict[Coord, str] = {}
+    for i in range(len(perimeter_ranges)):
+        range1 = perimeter_ranges[i]
+        range2 = perimeter_ranges[(i + 1) % len(perimeter_ranges)]
+
+        other1, corner = range1
+        _, other2 = range2
+
+        cx, cy = corner
+
+        rels = set()
+        for other in [other1, other2]:
+            ox, oy = other
+            if ox < cx and oy == cy:
+                rels.add("L")
+            elif ox > cx and oy == cy:
+                rels.add("R")
+            elif oy > cy:
+                rels.add("D")
+            else:
+                rels.add("U")
+
+        if rels == set("UL"):
+            corners_dict[corner] = "J"
+        elif rels == set("UR"):
+            corners_dict[corner] = "L"
+        elif rels == set("DL"):
+            corners_dict[corner] = "7"
+        else:
+            corners_dict[corner] = "F"
+
+    # Remember min x and max x so we can do a horizontal scan later.
+    all_xs: set[int] = set()
+    all_ys: set[int] = set()
+    for c1, c2 in perimeter_ranges:
+        x1, y1 = c1
+        x2, y2 = c2
+        all_xs.add(x1)
+        all_xs.add(x2)
+        all_ys.add(y1)
+        all_ys.add(y2)
+
+    # Remember 1 before and after 1 min/max so we start outside when doing a scan.
+    min_x = min(all_xs) - 2
+    max_x = max(all_xs) + 2
+
+    min_y = min(all_ys) - 2
+    max_y = max(all_ys) + 2
+
+    # Separate vertical and horizontal ranges for easier writing of logic later.
+    vertical_ranges: list[Range] = []
+    horizontal_ranges: list[Range] = []
+
+    for c1, c2 in perimeter_ranges:
+        if c1[1] == c2[1]:
+            horizontal_ranges.append((c1, c2))
+        else:
+            vertical_ranges.append((c1, c2))
+
+    @cache
+    def _is_in_vertical_range(target: Coord) -> bool:
+        """Do a thing."""
+
+        tx, ty = target
+
+        for c1, c2 in vertical_ranges:
+            x1, y1 = c1
+            _, y2 = c2
+
+            if tx == x1 and min((y1, y2)) <= ty <= max((y1, y2)):
+                return True
+
+        return False
+
+    @cache
+    def _is_in_horizontal_range(target: Coord) -> bool:
+        """Do a thing."""
+
+        tx, ty = target
+
+        for c1, c2 in horizontal_ranges:
+            x1, y1 = c1
+            x2, _ = c2
+
+            if ty == y1 and min((x1, x2)) <= tx <= max((x1, x2)):
+                return True
+
+        return False
+
+    # ======== DEBUG ==========
+
+    def _p(demo_coord):
+        for y in range(min_y, max_y):
+            for x in range(min_x, max_x):
+                asd = (x, y)
+                char = " "
+                if asd == demo_coord:
+                    char = "X"
+                elif asd in corners_dict:
+                    char = corners_dict[asd]
+                elif _is_in_horizontal_range(asd) or _is_in_vertical_range(asd):
+                    char = "#"
+                print(f"{char}", end="")
+            print()
+
+    if False:
+        _p(None)
+
+    # ======== DEBUG ==========
+
+    FLIP_PAIRS = set([frozenset("L7"), frozenset("FJ")])
+
+    @cache
+    def _is_in_shape(target: Coord) -> bool:
+        """Do a thing."""
+
+        # A coord is in the shape if it's directly in any of the ranges
+        if _is_in_horizontal_range(target):
+            return True
+
+        if _is_in_vertical_range(target):
+            return True
+
+        # Otherwise we need to scan left to right and every time we cross
+        # a vertical range we know we toggle between outside and inside the shape.
+        # Keep doing that until we reach our target coordinate.
+
+        tx, ty = target
+
+        is_inside = False
+        corners: list[str] = []
+
+        for test_x in range(min_x, max_x):
+            test_coord = (test_x, ty)
+
+            if test_x == tx:
+                # if not is_inside:
+                #     _p(test_coord)
+                return is_inside
+
+            # Crossing a vertical range means we potentially toggle
+            if _is_in_vertical_range(test_coord):
+                # If we're on a horizontal range too, we need to check the
+                # corners of the attached vertical ranges to see if we toggle
+                # in/out or not.
+                if _is_in_horizontal_range(test_coord):
+                    if test_coord in corners_dict:
+                        if not corners:
+                            corners.append(corners_dict[test_coord])
+                        else:
+                            char = corners_dict[test_coord]
+                            other_char = corners.pop()
+                            pair = frozenset(f"{char}{other_char}")
+                            if pair in FLIP_PAIRS:
+                                is_inside = not is_inside
+
+                # If we're not also on a horizontal range, we can just cleanly toggle
+                else:
+                    is_inside = not is_inside
+
+        err_msg = f"Shouldn't get here. {target}"
+        raise ValueError(err_msg)
+
+    return _is_in_shape
+
+
+def _iter_coords(c1, c2):
+    x1, y1 = c1
+    x2, y2 = c2
+
+    x1, x2 = tuple(sorted((x1, x2)))
+    y1, y2 = tuple(sorted((y1, y2)))
+
+    for y in range(y1, y2 + 1):
+        for x in range(x1, x2 + 1):
+            yield (x, y)
 
 
 @aoc_output_formatter(YEAR, DAY, 2, PART_TWO_DESCRIPTION, assert_answer=PART_TWO_ANSWER)
 def part_two(raw_input: list[str]) -> int | str | None:
-    pass
+    coords: list[Coord] = _parse(raw_input)
+
+    perimeter_ranges: list[Range] = []
+    for i in range(len(coords)):
+        c1, c2 = coords[i], coords[(i + 1) % len(coords)]
+        perimeter_ranges.append((c1, c2))
+
+    is_in_shape = _get_is_in_shape_fn(perimeter_ranges)
+
+    # tests = [
+    #     ((7, 1), True),
+    #     ((8, 1), True),
+    #     ((12, 1), False),
+    #     ((11, 1), True),
+    #     ((11, 5), True),
+    #     ((11, 8), False),
+    # ]
+
+    # for t, expected in tests:
+    #     calculated = is_in_shape(t)
+    #     mark = "" if calculated == expected else "  *****"
+    #     print(f"{t}: {expected=}, {calculated=}{mark}")
+
+    m = -1
+    for c1, c2 in combinations(coords, 2):
+        x1, y1 = c1
+        x2, y2 = c2
+
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+
+        # if the rect defined by these two corners is bigger than the
+        # previous max, then we check if it's entirely contained in the
+        # enclosed area, to save on runtime
+
+        new_size = (dx + 1) * (dy + 1)
+        if new_size > m:
+            # if every spot on the perimeter of the proposed rectangle is inside
+            # the larger shape, it's valid.
+
+            #   c1 ---> c3
+            #   c4 <--- c2
+
+            c3 = (x1, y2)
+            c4 = (x2, y1)
+
+            is_invalid = False
+
+            for nc in _iter_coords(c1, c3):
+                if not is_in_shape(nc):
+                    is_invalid = True
+                    break
+
+            if is_invalid:
+                continue
+
+            for nc in _iter_coords(c3, c2):
+                if not is_in_shape(nc):
+                    is_invalid = True
+                    break
+
+            if is_invalid:
+                continue
+
+            for nc in _iter_coords(c2, c4):
+                if not is_in_shape(nc):
+                    is_invalid = True
+                    break
+
+            if is_invalid:
+                continue
+
+            for nc in _iter_coords(c4, c1):
+                if not is_in_shape(nc):
+                    is_invalid = True
+                    break
+
+            if is_invalid:
+                continue
+
+            # yay, valid
+            m = new_size
+
+    return m
 
 
 def run(input_file: str) -> None:
